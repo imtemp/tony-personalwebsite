cbdc26f6e0ec21299261654ee8fb226ef87d7494
imtemp <t.lin0919@gmail.com>
2022-08-15 15:39:35 -0400

    installed necessary libraries and frameworks
    Created backend with Express
    Installed Tailwind, Bootstrap for front end

---
+++ tony-personalwebsite-ui/src/Component/About/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>Hello! I am Tony Lin</h1>
+      <h2>I am a Full-Stack Developer</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempor
+        massa sed odio hendrerit, nec lacinia nisi aliquet. Donec eu metus et
+        felis egestas fermentum nec eu libero. Fusce aliquam tellus quis ligula
+        suscipit, dignissim congue justo sollicitudin. In sodales egestas
+        tincidunt. Integer venenatis porttitor ipsum, pellentesque tempor sem
+        condimentum et. Pellentesque id risus nec ligula lacinia iaculis sit
+        amet vitae diam. Nulla tristique tincidunt est sed euismod. Fusce sit
+        amet mauris tempus, mollis nisi vel, tempus ipsum. Quisque faucibus enim
+        sit amet ligula ornare placerat. Phasellus rutrum neque vel efficitur
+        aliquam. Vestibulum at interdum mi, quis dapibus dolor. In viverra,
+        lectus vel aliquet scelerisque, elit turpis hendrerit odio, ut pharetra
+        nisl diam a sapien.
+      </p>
+    </div>
+  );
+};
+
+export default About;
