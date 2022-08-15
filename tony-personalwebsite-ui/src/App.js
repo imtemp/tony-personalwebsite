730b041038d108fcd382910ecd67af33d745e4cc
imtemp <t.lin0919@gmail.com>
2022-08-15 16:05:45 -0400

    changed folder structure

--- tony-personalwebsite-ui/src/App.js
+++ tony-personalwebsite-ui/src/App.js
@@ -1,12 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Component/Navbar/Navbar.js";
 import About from "./Component/About/About.js";
+import Skills from "./Component/Skills/Skills.js";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <About />
+      <About name="Bono" />
+      <Skills />
     </div>
   );
 }
