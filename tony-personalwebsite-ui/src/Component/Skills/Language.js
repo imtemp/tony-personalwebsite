29b81ac5ce32c67697b83462017fe4ab8ab84e60
imtemp <t.lin0919@gmail.com>
2022-08-17 15:28:03 -0400

    fixed text alignment and added hovers to skills

--- tony-personalwebsite-ui/src/Component/Skills/Language.js
+++ tony-personalwebsite-ui/src/Component/Skills/Language.js
@@ -1,10 +1,28 @@
+import React, { useState } from "react";
 import "./Language.css";
 
 const Language = (props) => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = (e) => {
+    e.preventDefault();
+    setHover(true);
+  };
+
+  const onHoverOver = (e) => {
+    e.preventDefault();
+    setHover(false);
+  };
   return (
     <div className="imageBox">
-      <img src={props.title}></img>
+      <img
+        className="img-language"
+        onMouseEnter={(e) => onHover(e)}
+        onMouseLeave={(e) => onHoverOver(e)}
+        src={props.title}
+      ></img>
       <p className="invis">123456679</p>
+      {hover && <p className="centered">{props.name}</p>}
     </div>
   );
 };
