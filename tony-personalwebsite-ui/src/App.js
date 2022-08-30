3f4f4bad8e0d4c6052de602955a4641e1934e6ce
imtemp <t.lin0919@gmail.com>
2022-08-29 22:09:38 -0400

    added footer

--- tony-personalwebsite-ui/src/App.js
+++ tony-personalwebsite-ui/src/App.js
@@ -16,6 +16,7 @@ function App() {
       <About name="Tony" />
       <Skills />
       <Projects />
+      <Footer />
     </div>
   );
 }
---
+++ tony-personalwebsite-ui/src/Component/Footer/Footer.js
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <div>
+      <h1>
+        Placeholder Footer, putting my to-do list for this website as it is
+        still a work in progress
+      </h1>
+      <ul>
+        <li>Style this footer</li>
+        <li>add functionality to navbar</li>
+        <li>
+          Enhance styling by adding better colour schemes, images and animations
+        </li>
+        <li>Implement back-end logic once front-end is complete</li>
+      </ul>
+    </div>
+  );
+};
