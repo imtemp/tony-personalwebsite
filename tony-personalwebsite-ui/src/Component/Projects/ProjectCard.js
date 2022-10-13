ba0ccf821a8c454301732cdd6e6ba20e18e40f4d
imtemp <t.lin0919@gmail.com>
2022-10-12 23:52:51 -0400

    updated css

--- tony-personalwebsite-ui/src/Component/Projects/ProjectCard.js
+++ tony-personalwebsite-ui/src/Component/Projects/ProjectCard.js
@@ -1,17 +1,20 @@
 import Button from "react-bootstrap/Button";
+import "./ProjectCard.css";
 import Card from "react-bootstrap/Card";
 
 function ProjectCard(props) {
   return (
-    <a href={props.link} className="no-underline text-black">
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="" />
-        <Card.Body>
-          <Card.Title>{props.title}</Card.Title>
-          <Card.Text>{props.description}</Card.Text>
-        </Card.Body>
-      </Card>
-    </a>
+    <div className="card">
+      <a href={props.link} className="no-underline text-black">
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="" />
+          <Card.Body>
+            <Card.Title>{props.title}</Card.Title>
+            <Card.Text>{props.description}</Card.Text>
+          </Card.Body>
+        </Card>
+      </a>
+    </div>
   );
 }
 
