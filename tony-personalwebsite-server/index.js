104f48b5de9d21417946056ca7fb447d5a942fde
imtemp <t.lin0919@gmail.com>
2022-10-22 21:37:29 -0400

    implemented backend

--- tony-personalwebsite-server/index.js
+++ tony-personalwebsite-server/index.js
@@ -1,7 +1,10 @@
 const Express = require("express");
-
+const path = require("path");
 const app = Express();
 
 app.listen("3000", () => {
   console.log("Listening on port 3000...");
 });
+
+//serve
+const publicpath = path.join();
